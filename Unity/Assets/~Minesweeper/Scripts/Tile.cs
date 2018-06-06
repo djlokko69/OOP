817f593d1b889231d3f15810fc19e0da48767083
chris lockhart <dj_lokko666@y7mail.com>
2018-06-06 10:05:57 +1000

    MINESWEEPER: Flag in Tile script

--- Unity/Assets/~Minesweeper/Scripts/Tile.cs
+++ Unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -11,10 +11,13 @@ namespace Minesweeper
         public int x, y;
         public bool isMine = false; // Is the current tile a mine?
         public bool isRevealed = false; // Has the tile already been revealed?
+        public bool isFlagged = false;
+        public bool unFlagged = false;
         [Header("Reference")]
         public Sprite[] emptySpirtes; // List of empty sprites i.e, empty, 1, 2, 3, etc...
         public Sprite[] mineSprites; // The mine sprites
         private SpriteRenderer rend; // Reference to sprite renderer
+        private Sprite[] flagSprites;
         #endregion
 
         void Awake()
@@ -42,6 +45,18 @@ namespace Minesweeper
             {
                 // Sets sprite to appropriate texture based on adjacent mines
                 rend.sprite = emptySpirtes[adjacentMines];
+            }
+        }
+        public void Flag(int adjacentFlags, int flagState = 0)
+        {
+            unFlagged = true;
+            if (isFlagged)
+            {
+                rend.sprite = flagSprites[flagState];
+            }
+            else
+            {
+                rend.sprite = emptySpirtes[adjacentFlags];
             }
         }
     }
