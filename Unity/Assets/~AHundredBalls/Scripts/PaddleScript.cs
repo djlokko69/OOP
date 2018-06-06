40a2416de3d7f13c3054a555e28834041cbf26f2
chris lockhart <dj_lokko666@y7mail.com>
2018-06-06 11:54:31 +1000

    100BALLZ: bucket n script paddle script done

--- Unity/Assets/~AHundredBalls/Scripts/PaddleScript.cs
+++ Unity/Assets/~AHundredBalls/Scripts/PaddleScript.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaddleScript : MonoBehaviour {
+public class PaddleScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>(); // Get the animator component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow)) // Check if down arrow is pressed
+        {
+            // Modify parameter we created earlier
+            anim.SetBool("IsOpened", true);
+        }
+        else // If the button isn't pressed
+        {
+            // Set that parameter to false
+            anim.SetBool("IsOpened", false);
+        }
+    }
 }
